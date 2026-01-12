7009ec6065f021c7d2b569e59aeef06bb973caea
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-12 14:30:37 -0600

    HOPE WHEN THE WATER RISES, YOU BUILT A WALL
    Update the markAsDone function to modify todo items.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
 function markAsDone(todos) {
     while (todos < 5){
     todos ++;
-    }
+    todos[i] = 'done - ' + todos[i];
+ }
+ return todos;
 }
 
 document.getElementById('result').innerHTML = markAsDone(todos);
-
