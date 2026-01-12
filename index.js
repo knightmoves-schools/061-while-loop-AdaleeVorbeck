2a34dcc1a695615b1937655173a97260385af8bd
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-12 14:26:06 -0600

    SOMETHINGS GOTTA BREAK, SOMETHINGS GOTTA GIVE
    Initialize todos array and implement markAsDone function.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
+
+function markAsDone(todos) {
+    while (todos < 5){
+    todos ++;
+    }
+}
+
+document.getElementById('result').innerHTML = markAsDone(todos);
 
