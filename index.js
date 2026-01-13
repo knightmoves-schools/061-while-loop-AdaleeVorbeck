beaa41e3ed063e94ee8892dd7e2afc5cf2898c02
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-13 14:02:29 -0600

    I WILL LEAVE THE LIGHT ON

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
 
 function markAsDone(todos) {
-    while (todos < 5){
-    todos ++;
-    todos[i] = 'done - ' + todos[i];
+    let i = 0;
+    while (i < todos.length){
+    todos[i] = "done - " + todos[i];
+    i++;
  }
- return todos;
+ return todos.join('<br>');
 }
 
 document.getElementById('result').innerHTML = markAsDone(todos);
+
